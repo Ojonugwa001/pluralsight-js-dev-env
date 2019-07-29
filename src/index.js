a3a62f80e76bf4710215466253816534d72b695d
Alikali Ojonugwa <ojonugwajusticealikali@gmail.com>
2019-07-29 17:49:18 +0100

    module 9 work in progress

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './index.css';
+
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I Would pay ${courseValue} for this awesome course!`);
+debugger; // eslint-disable-line no-debugger
+console.log(`I Would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
